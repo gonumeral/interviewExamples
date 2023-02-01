5b1e4fbb0da47939ce87147de39831c2cc73e46a
Juan Gonzalez <juan@gonumeral.com>
2023-02-01 10:34:13 -0800

    add python/java remove ruby/node for now

---
+++ java/WeatherData.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class WeatherData {
+    public static void main(String[] args) {
+        String openWeatherKey = "42549e790dc5be9f25740b93acb26413";
+        String geoTagUrl = "http://api.openweathermap.org/geo/1.0/direct?q=San Francisco&appid=" + openWeatherKey;
+        try {
+            URL url = new URL(geoTagUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                JSONArray data = new JSONArray(response.toString());
+                double lon = data.getJSONObject(0).getDouble("lon");
+                double lat = data.getJSONObject(0).getDouble("lat");
+                String weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + openWeatherKey;
+                url = new URL(weatherUrl);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    response = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+                    JSONObject weatherData = new JSONObject(response.toString());
+                    double temperature = weatherData.getJSONObject("main").getDouble("temp");
+                    System.out.println("Temperature: " + temperature);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- python3/app.py
+++ python3/app.py
@@ -1,11 +1,14 @@
-import time
-import urllib.request
-import urllib.parse
+import requests
+import json
 
-now_seconds = time.time()
-hashtag = "#foo"
-url = "http://interview.gonumeral.com/search?q=" + urllib.parse.quote(hashtag)
-url = url + "&beforeTimeSecondsInclusive=" + str(now_seconds + 3600)
-url = url + "&afterTimeSecondsExclusive=" + str(now_seconds - 3600 * 2)
-contents = urllib.request.urlopen(url).read()
-print(contents)
+
+open_weather_key = "MY_API_KEY"
+geo_tag_url = "http://api.openweathermap.org/geo/1.0/direct?q=%s&appid=%s" % ("San Francisco", open_weather_key )
+response = requests.get(geo_tag_url)
+data = json.loads(response.text)
+lon = data[0]['lon']
+lat = data[0]['lat']
+weather_url="https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=%s&appid=%s" %(lat,lon,"imperial",open_weather_key)
+response = requests.get(weather_url)
+data = json.loads(response.text)
+print(data["main"]["temp"])
